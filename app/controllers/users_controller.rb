16d92e953dd4dd8415a224bdfec454240152ff32
github username <github email address>
2021-08-15 13:48:21 -0500

    updated login sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
 
     get '/login' do
-        
         erb :login
+    end
+
+    post '/login' do
+        @user = User.find_by(email :params[:email])
+        @user.authenticate(params[:password])
+    end
+
+    get '/signup' do
+    
     end
 
 end
