7f4cc7b2246aac3c68f897a8d9b3cdb321d00833
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-09-26 20:43:15 -0500

    flash messages in pupdates controller

--- app/controllers/pupdates_controller.rb
+++ app/controllers/pupdates_controller.rb
@@ -6,23 +6,20 @@ class PupdatesController < ApplicationController
     end
     
     get '/pupdates/new' do
+        redirect_if_not_logged_in
         erb :'pupdates/new'
     end
 
     post '/pupdates' do
-        if !logged_in?
-            redirect '/'
-    end
-
+        redirect_if_not_logged_in
     if params[:content] != ""
         @pupdate = Pupdate.create(content: params[:content], user_id: current_user.id, title: params[:title], mood: params[:mood])
-        flash[:message] = "Pupdate saved." if @pupdate.id
+        flash[:message] = "Pupdate created." if @pupdate.id
         redirect "/pupdates/#{pupdate.id}"
 
     else
         flash[:errors] = "Please provide content for your pupdate."
         redirect '/pupdates/new'
-
     end
 end
 
@@ -32,36 +29,32 @@ end
     end
 
     get '/pupdates/:id/edit' do
+        redirect_if_not_logged_in
         set_pupdate
-        if logged_in?
-            if authorized_to_edit(@pupdate)
-                erb :'pupdates/edit'
-            else
-                redirect "users/#{current_user.id}"
-            end
+        if authorized_to_edit(@pupdate)
+            erb :'pupdates/edit'
         else
-            redirect '/'
+            redirect "users/#{current_user.id}"
         end
     end
+     
 
     patch '/pupdates/:id' do
+        redirect_if_not_logged_in
         set_pupdate
-        if logged_in?
             if @pupdate.user == current_user 
                 @pupdate.update(content: params[:content])
                 redirect "pupdates/#{pupdate.id}"
             else
                 redirect "users/#{current_user.id}"
             end
-        else
-            redirect '/'
         end
-    end
 
     delete 'pupdates/:id' do
         set_pupdate
         if authorized_to_edit?(@pupdate)
             @pupdate.destroy
+            flash[:message] = "Pudate deleted."
             redirect '/pupdates'
         else
             redirect '/pupdates'
